647af97294b2df1f997f229dff8a087d0d2a2e6c
aoba0826 <werwerowner@gmail.com>
2022-05-18 05:48:21 +0000

    楽天API フォーム追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,12 @@ class HomesController < ApplicationController
   def top
   end
 
+  def search_rakuten_api(keyword)
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+      render :top
+  end
+
+
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to:'homes#top'
   get 'homes/about'
   devise_for :users
+  get "search_rakuten_api" => "homes#search_rakuten_api"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
